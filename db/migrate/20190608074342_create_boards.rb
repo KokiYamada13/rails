fbd108b60a338a4f55e93af54e7b03547b2f6453
Yamada Koki <lenny.srv.826@gmail.com>
2019-06-08 17:12:57 +0900

    1st  migrate

--- db/migrate/20190608074342_create_boards.rb
+++ db/migrate/20190608074342_create_boards.rb
@@ -1,6 +1,6 @@
 class CreateBoards < ActiveRecord::Migration[5.0]
   def change
-    create_table :boards do |t|
+    create_table :boards do |t|  #テーブルを作成する。下のブロックの中で定義。|t|は引数。
       t.string :name
       t.string :title
       t.text :body
@@ -9,3 +9,7 @@ class CreateBoards < ActiveRecord::Migration[5.0]
     end
   end
 end
+
+
+#このファイルはDBにbordsテーブルを作成する為の定義が記載されている。
+#マイグレーションファイル作成後、マイグレーションコマンドを実行して作成される。
