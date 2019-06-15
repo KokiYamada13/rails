51e8ffc6501cda9c7d639d4c127e20c5b5637cd6
Yamada Koki <lenny.srv.826@gmail.com>
2019-06-15 16:25:36 +0900

    eddit  routes & link

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root 'boards#index'
-  get 'boards', to: 'boards#index'   #GETメソッドで/boardsにリクエストが来た場合、indexメソッドが呼ばれる。
-  get 'boards/new', to: 'boards#new'    #GETメソッドで/boards/newにリクエストが来た場合、newメソッドが呼ばれる。
-  post 'boards', to: 'boards#create'
-  get 'boards/:id', to: 'boards#show'  #idが入る部分は　:idとする
+  
+  resources :boards, only: [:index, :new, :create, :show]   # 個々にルートを作成したが、リソースベースルーティングで基本のルーティングを１行で済ませる。onlyで指定する。
+
+
+  #get 'boards', to: 'boards#index'   #GETメソッドで/boardsにリクエストが来た場合、indexメソッドが呼ばれる。
+  #get 'boards/new', to: 'boards#new'    #GETメソッドで/boards/newにリクエストが来た場合、newメソッドが呼ばれる。
+  #post 'boards', to: 'boards#create'
+  #get 'boards/:id', to: 'boards#show'  #idが入る部分は　:idとする
 end
