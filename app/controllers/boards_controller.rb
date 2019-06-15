6483f8e8bf4daaa64f8aac93fbeb9b73bff7d7fc
Yamada Koki <lenny.srv.826@gmail.com>
2019-06-15 14:24:34 +0900

    add Create

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,8 +1,19 @@
 class BoardsController < ApplicationController
   def index
+    @boards = Board.all #allは全て取得することになる。（開発時のみ。）
   end
 
   def new
-    @board = Board.new
+    @board = Board.new   
+  end
+
+  def create
+    Board.create(board_params) #保存
+  end
+
+  private  #プライベートメソッド
+
+  def board_params
+    params.require(:board).permit(:name, :title, :body) #requireメソッドでモデル名のキーを取得し、permitメソッドでそのモデルのキーを許可する。
   end
 end
