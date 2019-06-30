f0c1304f5deea002f15e4634779abc3ceae1c536
Yamada Koki <lenny.srv.826@gmail.com>
2019-06-30 16:08:50 +0900

    add routes comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root 'boards#index'
-  
-  resources :boards  #only: [:index, :new, :create, :show, :edit, :update]   # 個々にルートを作成したが、リソースベースルーティングで基本のルーティングを１行で済ませる。onlyで指定する。
 
+  resources :boards  #only: [:index, :new, :create, :show, :edit, :update]   # 個々にルートを作成したが、リソースベースルーティングで基本のルーティングを１行で済ませる。onlyで指定する。
+  resources :comments, only: %i[create destroy]
 
   #get 'boards', to: 'boards#index'   #GETメソッドで/boardsにリクエストが来た場合、indexメソッドが呼ばれる。
   #get 'boards/new', to: 'boards#new'    #GETメソッドで/boards/newにリクエストが来た場合、newメソッドが呼ばれる。
