4cd0f91270f8763b77a2876c2ca41cf2d6da443b
Yamada Koki <lenny.srv.826@gmail.com>
2019-07-04 22:16:55 +0900

    set route users

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/me'
-
-  get 'home/index'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
+  get 'mypage', to: 'users#me'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root 'boards#index'
 
   root 'home#index'
+  resources :users, only: %i[new create]
   resources :boards  #only: [:index, :new, :create, :show, :edit, :update]   # 個々にルートを作成したが、リソースベースルーティングで基本のルーティングを１行で済ませる。onlyで指定する。
   resources :comments, only: %i[create destroy]
 
